cc59ec83c06623c778ea31ad4c0effdcb161fce7
wangqianqian1100 <17343030061@163.com>
2020-01-13 12:04:48 +0800

    iview

---
+++ vue-iview/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <RadioGroup v-model="phone">
+          <Radio label="apple">
+              <Icon type="logo-apple"></Icon>
+              <span>Apple</span>
+          </Radio>
+          <Radio label="android">
+              <Icon type="logo-android"></Icon>
+              <span>Android</span>
+          </Radio>
+          <Radio label="windows">
+              <Icon type="logo-windows"></Icon>
+              <span>Windows</span>
+          </Radio>
+      </RadioGroup>
+      <RadioGroup v-model="animal">
+          <Radio label="金斑蝶"></Radio>
+          <Radio label="爪哇犀牛"></Radio>
+          <Radio label="印度黑羚"></Radio>
+      </RadioGroup>
+  </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                phone: 'apple',
+                animal: '爪哇犀牛'
+            }
+        }
+    }
+</script>
